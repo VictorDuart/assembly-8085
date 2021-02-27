e2b873bae68d559b746e6afdbdf632717b562510
VictorDuart <victorhd2014@gmail.com>
2021-02-27 14:07:49 -0300

    comment update

--- sub-num-16bits.asm
+++ sub-num-16bits.asm
@@ -1,6 +1,8 @@
 # ORG 2000
 # BEGIN 2000
 //Algoritmo realiza a subtração de dois números de 16 bits
+//Os dados de entrada são salvos nos espaços 2050, 2051, 2052 e 2053 da memória
+//Os dados de saída são salvos nos espaços 2060 e 2061 da memória
 	   LXI H,4030
 	   SHLD 2050
 	   LXI H,2010

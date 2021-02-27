fb1dc59c478e91e3c345abe3ec7eb308cc62ec18
VictorDuart <victorhd2014@gmail.com>
2021-02-27 14:04:52 -0300

    comment update

--- add-num-16bits.asm
+++ add-num-16bits.asm
@@ -1,6 +1,8 @@
 # ORG 2000
 # BEGIN 2000
 //Algoritmo para realizar a soma de dois números de 16 bits
+//Os dados de entrada são salvos nos espaços 2050, 2051, 2052 e 2053 da memória
+//Os dados de saída são salvos nos espaços 2060 e 2061 da memória
 	   LXI H,2010
 	   SHLD 2050
 	   LXI H,4030

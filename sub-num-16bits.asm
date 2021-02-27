eb235699fc171f998b75211dd784a872611f239b
VictorDuart <victorhd2014@gmail.com>
2021-02-27 13:59:31 -0300

    added subtrator-16bits

---
+++ sub-num-16bits.asm
@@ -0,0 +1,16 @@
+# ORG 2000
+# BEGIN 2000
+//Algoritmo realiza a subtração de dois números de 16 bits
+	   LXI H,4030
+	   SHLD 2050
+	   LXI H,2010
+	   SHLD 2052
+	   LDA 2050
+	   LXI H,2052
+	   SUB M
+	   STA 2060
+	   LDA 2051
+	   LXI H,2053
+	   SUB M
+	   STA 2061
+	   HLT

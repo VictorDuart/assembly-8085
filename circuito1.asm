9628a411271f3fa1a1141f5bc7a227071822fe6e
VictorDuart <victorhd2014@gmail.com>
2021-03-10 02:01:22 -0300

    update circuito1

--- circuito1.asm
+++ circuito1.asm
@@ -1,7 +1,7 @@
 // Algoritmo implementa a expressão lógica do Circuito1
 // Entrada M armazenada no espaço 4000 da memória
 // Entrada N armazenada no espaço 4001 da memória
-// Entrada Q armazenada no espaço 4002 da memória 
+// Entrada Q armazenada no espaço 4002 da memória
 // Saída armazenada no espaço 4050 da memória
 # ORG 3000
 # BEGIN 3000
@@ -12,6 +12,8 @@
 	   LXI H,4002
 	   MVI M,01
 	   LDA 4000
+	   LXI H,4001
+	   ORA M
 	   LXI H,4002
 	   ANA M
 	   STA 4050

6cb66d691aa528adcfa53afd65d984167c4bc7e5
VictorDuart <victorhd2014@gmail.com>
2021-03-17 15:23:46 -0300

    Added multiplica-8bits.asm

---
+++ multiplica-8bits.asm
@@ -0,0 +1,17 @@
+// multiplicação de dois números de 8 bits
+// in -> reg C * reg D
+// out -> resultado reg E
+# ORG 2000
+# BEGIN 2000
+	   MVI C,02
+	   MVI D,03
+	   CALL MULTI
+	   MOV E,A
+	   HLT
+
+MULTI:	   MOV A,D
+	   ADD B
+	   MOV B,A
+	   DCR C
+	   JNZ MULTI
+	   RZ

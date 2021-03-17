31212bacc92ce30f00b133b23100c707f1594662
VictorDuart <victorhd2014@gmail.com>
2021-03-17 14:49:52 -0300

    added divisao-8bits

---
+++ divisao-8bits.asm
@@ -0,0 +1,14 @@
+//divisão de dois numeros de 8 bits
+//IN -> reg A / reg B
+//OUT -> resultado = reg C, resto = reg D
+# ORG 2000
+# BEGIN 2000
+	   MVI A,07
+	   MVI B,02
+	   MVI C,00
+
+LOOP:	   SUB B
+	   CMP B
+	   INR C
+	   JNC LOOP
+	   MOV D,A
